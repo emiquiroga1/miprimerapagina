27f2fa3410a48ce2096398b95e215bd82a4d131a
emiquiroga1 <e.e_q@hotmail.com>
2023-06-05 23:49:17 -0300

    miprimerapagina

---
+++ index.js
@@ -0,0 +1,4 @@
+const miboton = document.getElementById('miboton');
+miboton.addEventListener("click", function handleclick(){
+    alert('!hola! has hecho clic en el boton.');
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert(){ alert ('!Hola, esta es una alerta desde Jacascript!');}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
